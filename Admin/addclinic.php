1929f3057376cd2d0003f866cbcf8bbfa9f3268c
stalin655 <stalinmortice655@gmail.com>
2023-06-06 11:23:12 +0300

    updates

--- Admin/addclinic.php
+++ Admin/addclinic.php
@@ -2,64 +2,7 @@
 <head>
 <link rel="stylesheet" href="adminmain.css"> 
 </head>
-<body  style="background-color:#0092ca" behavior="fixed">
-<ul >
-<li class="dropdown"><font text-align="center" color="white" size="6">ADMIN MODE</font></li>
-<br>
-<h2>
-  <li class="dropdown">    
-  <a href="javascript:void(0)" class="dropbtn">Doctor</a>
-    <div class="dropdown-content">
-      <a href="adddoctor.php">Add Doctor</a>
-      <a href="deletedoctor.php">Delete Doctor</a>
-      <a href="showdoctor.php">Show Doctor</a>
-	  <a href="showdoctorschedule.php">Show Doctor Schedule</a>
-    </div>
-  </li>
-  
-  <li class="dropdown">
-  <a href="javascript:void(0)" class="dropbtn">Clinic</a>
-    <div class="dropdown-content">
-      <a href="addclinic.php">Add Clinic</a>
-      <a href="deleteclinic.php">Delete Clinic</a>
-      <a href="adddoctorclinic.php">Assign Doctor to Clinic</a>
-	  <a href="addmanagerclinic.php">Assign Manager to Clinic</a>
-	  <a href="deletedoctorclinic.php">Delete Doctor from Clinic</a>
-	  <a href="deletemanagerclinic.php">Delete Manager from Clinic</a>
-	  <a href="showclinic.php">Show Clinic</a>
-    </div>
-  </li>
-  <li class="dropdown">    
-  <a href="javascript:void(0)" class="dropbtn">Manager</a>
-    <div class="dropdown-content">
-      <a href="addmanager.php">Add Manager</a>
-      <a href="deletemanager.php">Delete Manager</a>
-	  <a href="showmanager.php">Show Manager</a>
-    </div>
-  </li>
-   <li>  
-	<form method="post" action="mainpage.php">	
-	<button type="submit" class="cancelbtn" name="logout" style="float:right;font-size:22px"><b>Log Out</b></button>
-	</form>
-  </li>
-	
-</ul>
-</h2>
-<center><h1 style="background-color:#0092ca">ADD CLINIC</h1><hr>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  CID:<input type="number" name="cid" required>
-  <br>
-  Name: <input type="text" name="name" required>
-  <br>
-  Address: <input type="text" name="address" required>
-  <br>
-  Town: <input type="text" name="town" required>
-  <br>
-  Contact no.: <input type="number" name="contact" maxlength="10" minlength="10" required>
-  <button type="submit" name="Submit">REGISTER</button>
-</form>
-</font></b>
-</center>
+
 <?php
 session_start();
 if(isset($_POST['logout'])){
@@ -118,6 +61,65 @@ if(isset($_POST['Submit']))
 }
 
 ?>
-
+<body  style="background-color:#0092ca" behavior="fixed">
+<ul >
+<li class="dropdown"><font text-align="center" color="white" size="6">ADMIN MODE</font></li>
+<br>
+<h2>
+  <li class="dropdown">    
+  <a href="javascript:void(0)" class="dropbtn">Doctor</a>
+    <div class="dropdown-content">
+      <a href="adddoctor.php">Add Doctor</a>
+      <a href="deletedoctor.php">Delete Doctor</a>
+      <a href="showdoctor.php">Show Doctor</a>
+	  <a href="showdoctorschedule.php">Show Doctor Schedule</a>
+    </div>
+  </li>
+  
+  <li class="dropdown">
+  <a href="javascript:void(0)" class="dropbtn">Clinic</a>
+    <div class="dropdown-content">
+      <a href="addclinic.php">Add Clinic</a>
+      <a href="deleteclinic.php">Delete Clinic</a>
+      <a href="adddoctorclinic.php">Assign Doctor to Clinic</a>
+	  <a href="addmanagerclinic.php">Assign Manager to Clinic</a>
+	  <a href="deletedoctorclinic.php">Delete Doctor from Clinic</a>
+	  <a href="deletemanagerclinic.php">Delete Manager from Clinic</a>
+	  <a href="showclinic.php">Show Clinic</a>
+    </div>
+  </li>
+  <li class="dropdown">    
+  <a href="javascript:void(0)" class="dropbtn">Manager</a>
+    <div class="dropdown-content">
+      <a href="addmanager.php">Add Manager</a>
+      <a href="deletemanager.php">Delete Manager</a>
+	  <a href="showmanager.php">Show Manager</a>
+    </div>
+  </li>
+   <li>  
+	<form method="post" action="mainpage.php">	
+	<button type="submit" class="cancelbtn" name="logout" style="float:right;font-size:22px"><b>Log Out</b></button>
+	</form>
+  </li>
+	
+</ul>
+</h2>
+<center><h1 style="background-color:#0092ca">ADD CLINIC</h1><hr>
+<form style="background-color:#0092ca" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  CID:<input type="number" name="cid" required>
+  <br>
+  Name: <input type="text" name="name" required>
+  <br>
+  Address: <input type="text" name="address" required>
+  <br>
+  Town: <input type="text" name="town" required>
+  <br>
+  City: <input type="text" name="city" required>
+  <br>
+  Contact no.: <input type="number" name="contact" maxlength="10" minlength="10" required>
+  <button type="submit" name="Submit">REGISTER</button>
+</form>
+</font></b>
+</center>
 </body>
 </html>
